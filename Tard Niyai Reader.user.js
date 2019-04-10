89d79ee2fbb2b76036019b3b78ac86e1f23543d5
T5ive <41800233+T5ive@users.noreply.github.com>
2019-04-11 05:08:20 +0700

    Update Tard Niyai Reader.user.js

--- Tard Niyai Reader.user.js
+++ Tard Niyai Reader.user.js
@@ -8,17 +8,17 @@
 
 function getDivs(){
     return Array.from(document.getElementsByClassName('col-md-3'))
-        .concat(Array.from(document.getElementsByClassName('hidden-sm-down text-center')))
+    .concat(Array.from(document.getElementsByClassName('hidden-sm-down text-center')))
     .concat(Array.from(document.getElementsByClassName('desktop navbar navbar-bottom navbar-fixed-bottom')))
     .concat(Array.from(document.getElementsByClassName('fb-comments fb_iframe_widget fb_iframe_widget_fluid_desktop')))
 }
 
 function a () {
-    let premiumDivs = getDivs();
-    if (!premiumDivs.length){
+    let myDivs = getDivs();
+    if (!myDivs.length){
         setTimeout(a, 2000);
     } else {
-        premiumDivs.forEach(function(element) {
+        myDivs.forEach(function(element) {
             element.remove();
         });
     }
